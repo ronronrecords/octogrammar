337d8d287e6bbfc135bfbe4e3972ad93dfe51f74
RONRON Records <info@ronronrecords.com>
2017-12-13 11:01:49 -0800

    public files added

---
+++ Public/assets/javascript/model.js
@@ -0,0 +1,32 @@
+user = {
+    listofsong: [
+        {
+            Title: "",
+            sections: {                
+                Verse: ["",""],
+                Prechorus:["",""],
+                Chorus: ["",""],
+                Bridge: ["",""],
+                Outro: "",
+            },
+            timestamp: {
+                createdDate: "",
+                modifiedDate: "",
+            }
+        },
+        {
+            Title: "",
+            sections: {               
+                Verse: ["",""],
+                Prechorus:["",""],
+                Chorus: ["",""],
+                Bridge: ["",""],
+                Outro: "",
+            },
+            timestamp: {
+                createdDate: "",
+                modifiedDate: "",
+            }
+        }
+]
+}

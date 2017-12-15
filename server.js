d12d7935459e1eca85be76d70389d34a1cf87377
RONRON Records <info@ronronrecords.com>
2017-12-15 15:59:19 -0800

    Debugging handlebar routes for displaying html

--- server.js
+++ server.js
@@ -6,7 +6,9 @@
 //needed
 var express = require("express");
 var bodyParser = require("body-parser");
-var hbs = require("handlebars");
+var exphbs = require("express-handlebars");
+var path = require("path");
+// var hbs = require("handlebars");
 
 // Sets up the Express App
 // =============================================================
@@ -14,6 +16,14 @@ var hbs = require("handlebars");
 //needed
 var app = express();
 var PORT = process.env.PORT || 3308;
+var path = require("path");
+var options = { dotfiles: 'ignore', etag: false,
+extensions: ['htm', 'html'],
+index: false
+};
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 // Sets up the Express app to handle data parsing
 
@@ -28,8 +38,11 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory to be served**
 // Needed to serve images, CSS files, JS files**
 // Now files can be loaded http://localhost:3000/images/etc.etc**
-app.set("view engine", "hbs");
-app.use(express.static("public"));
+// app.set("view engine", "hbs");
+// app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, 'public'), options));
+
 
 // Routes
 //javascript file and module loader, improves speed and quality of code**
@@ -44,6 +57,6 @@ require("./routes/api-routes.js")(app);
 // Starts the server to begin listening**
 //creates a listener on the specified port or path**
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
